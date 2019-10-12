7b9efde16a88fb6a31bbae9e2ee3654a73353317
Tom <tom.marx@hotmail.fr>
2019-10-12 15:37:50 +0200

    sizeof

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: tmarx <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 14:24:45 by tmarx             #+#    #+#             */
-/*   Updated: 2019/10/10 14:24:46 by tmarx            ###   ########.fr       */
+/*   Updated: 2019/10/12 15:32:05 by tmarx            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list *res;
 
-	res = ft_calloc(sizeof(t_list *), 1);
+	res = ft_calloc(sizeof(t_list), 1);
 	if (!res)
 		return (NULL);
 	res->content = content;
